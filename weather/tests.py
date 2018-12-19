bb8d3577eb2bb2d4a4fb56cafd525fd04bba4d8a
Cindy Liu <jl7e12@gmail.com>
2018-12-19 23:23:53 +0000

    Manage to get the response from weather forecast API and filter the data, all the logic tested.

--- weather/tests.py
+++ weather/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from .views import get_weather_summary
+from .views import get_weather_forecast_temp_and_dt
+from .views import get_temps_for_tomorrow
+import datetime
 
 class WeatherTest(TestCase):
 
@@ -20,3 +23,111 @@ class WeatherTest(TestCase):
           'humidity': 76
         }
         self.assertEquals(get_weather_summary(weather_data), expected)
+
+    def test_get_weather_forecast_temp_and_dt(self):
+        weather_forecast_data = [
+          {
+            "dt": 1545177600,
+            "main": {
+              "temp": 7.92,
+              "temp_min": 7.92,
+              "temp_max": 8.38,
+              "pressure": 1007.99,
+              "sea_level": 1015.53,
+              "grnd_level": 1007.99,
+              "humidity": 98,
+              "temp_kf": -0.46
+            },
+            "weather": [
+              {
+                "id": 500,
+                "main": "Rain",
+                "description": "light rain",
+                "icon": "10n"
+              }
+            ],
+            "clouds": {
+              "all": 48
+            },
+            "wind": {
+              "speed": 4.45,
+              "deg": 252.012
+            },
+            "rain": {
+              "3h": 2.3875
+            },
+            "sys": {
+              "pod": "n"
+            },
+            "dt_txt": "2018-12-19 00:00:00"
+          },
+          {
+            "dt": 1545264000,
+            "main": {
+              "temp": 6.68,
+              "temp_min": 6.68,
+              "temp_max": 7.03,
+              "pressure": 1008.82,
+              "sea_level": 1016.43,
+              "grnd_level": 1008.82,
+              "humidity": 99,
+              "temp_kf": -0.34
+            },
+            "weather": [
+              {
+                "id": 800,
+                "main": "Clear",
+                "description": "clear sky",
+                "icon": "02n"
+              }
+            ],
+            "clouds": {
+              "all": 8
+            },
+            "wind": {
+              "speed": 4.01,
+              "deg": 216.001
+            },
+            "rain": {},
+            "sys": {
+              "pod": "n"
+            },
+            "dt_txt": "2018-12-20 00:00:00"
+          }
+        ]
+        expected = [
+          {
+            "temp": 7.92,
+            "dt": "2018-12-19 00:00:00"
+          },
+          {
+            "temp": 6.68,
+            "dt": "2018-12-20 00:00:00"
+          }
+        ]
+
+        self.assertEquals(get_weather_forecast_temp_and_dt(weather_forecast_data), expected)
+
+    def test_get_temps_for_tomorrow(self):
+        filtered_forecast_data = [
+          {
+            "temp": 7.92,
+            "dt": "2018-12-19 00:00:00"
+          },
+          {
+            "temp": 6.68,
+            "dt": "2018-12-20 00:00:00"
+          }
+        ]
+        expected = [
+          {
+            "temp": 6.68,
+            "dt": "2018-12-20 00:00:00"
+          }
+        ]
+
+        today = datetime.datetime(2018,12,19)
+
+        self.assertEquals(get_temps_for_tomorrow(filtered_forecast_data, today), expected)
+
+
--- weather/views.py
+++ weather/views.py
@@ -1,18 +1,27 @@
 import os
-
+import datetime
 import requests
 from django.shortcuts import render
 
 
 def index(request):
     api_key=os.environ['WEATHER_API_KEY']
+    today = datetime.datetime.today()
+
     response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid={api_key}')
     # TODO: handle extra errors
     weather_data = response.json()
 
-    weather_summary = get_weather_summary(weather_data)
+    response2 = requests.get(f'http://api.openweathermap.org/data/2.5/forecast?q=London,uk&units=metric&appid={api_key}')
+    # TODO: handle extra errors
+    weather_forecast_data = response2.json()
 
-    return render(request, 'index.html', weather_summary)
+    weather_summary = get_weather_summary(weather_data)
+    weather_forecast_summary = get_temps_for_tomorrow(get_weather_forecast_temp_and_dt(weather_forecast_data['list']), today)
+
+    return render(request, 'index.html', { 'weather_forecast_summary': weather_forecast_summary,
+                                           'weather_summary': weather_summary
+                                           })
 
 
 def get_weather_summary(weather_data):
@@ -22,3 +31,16 @@ def get_weather_summary(weather_data):
       'max': weather_data['main']['temp_max'],
       'humidity':weather_data['main']['humidity']
     }
+
+def get_weather_forecast_temp_and_dt(weather_forecast_data):
+    return list(map(lambda x: {
+        'temp': x['main']['temp'],
+        'dt': x['dt_txt']
+      }, weather_forecast_data))
+
+
+def get_temps_for_tomorrow(filtered_forecast_data, today):
+    tomorrow = str(today + datetime.timedelta(days = 1)).split(' ')[0]
+    print(today)
+
+    return list(filter(lambda x: x['dt'].split(' ')[0] == tomorrow, filtered_forecast_data ))
